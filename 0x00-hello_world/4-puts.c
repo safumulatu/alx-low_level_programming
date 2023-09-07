52b28699448270e90d509d3625753290fc4e9078
safumulatu <safumulatu9@gmail.com>
2023-09-07 02:19:42 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // Use the puts function to print the string
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate successful termination
+    return 0;
+}
