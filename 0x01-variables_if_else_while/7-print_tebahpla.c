6bbd560aff06348c5d3a25d25244988ec5676aae
safumulatu <safumulatu9@gmail.com>
2023-09-08 05:50:47 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - print alphabets without char and use putchar
+ * main - Prints the lowercase alphabet in reverse.
  *
- * Return: always (0)
- *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int alphabets = '0';
+	char letter;
 
-	while (alphabets <= '9')
-	{
-		putchar(alphabets);
-		alphabets++;
-	}
-		putchar('\n');
-		return (0);
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
