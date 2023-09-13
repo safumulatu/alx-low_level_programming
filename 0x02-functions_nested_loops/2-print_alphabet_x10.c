1fe66656afc6a16f0913ccc77e2d8a8ca7f9a984
safumulatu <safumulatu9@gmail.com>
2023-09-13 06:55:09 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
-#include "main.h"
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
 {
-	int test = 0;
-	char ro_letter;
+	int count = 0;
+	char letter;
 
-	while (test++ <= 9)
+	while (count++ <= 9)
 	{
-		for (ro_letter = 'a'; ro_letter <= 'z'; ro_letter++)
-			_putchar(ro_letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
