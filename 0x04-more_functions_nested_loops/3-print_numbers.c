8ec026027467fd9d2d17f36eb430a9e02e797277
safumulatu <safumulatu9@gmail.com>
2023-09-15 01:30:03 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - from 0 to 9
+* owned by safu mulatu
+*Return: void for it
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
