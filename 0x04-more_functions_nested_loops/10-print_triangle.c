4d301ac537540c133c0d6e72dbf74fa121778108
safumulatu <safumulatu9@gmail.com>
2023-09-15 02:39:01 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * owned by safu mulatu
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int R, H, S;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (R = 1; R <= size; R++)
+		{
+			for (S = size - R; S >= 1; S--)
+			{
+				_putchar(' ');
+			}
+			for (H = 1; H <= R; H++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	}
