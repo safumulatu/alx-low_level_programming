1118f84aa000f783e3b0c2dfb2d636c0aa4996be
safumulatu <safumulatu9@gmail.com>
2023-09-15 02:33:28 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print 1 to 100 and fizzbuss
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if ((x % 3) == 0 && (x % 5) == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", x);
+		}
+
+
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
