f18cc9e851adace3fb27993d2f2461f294ab8c3c
safumulatu <safumulatu9@gmail.com>
2023-09-18 03:52:10 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a and int b
+* Owned by Safu Mulatu
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
+		int safu;
+
+		safu = *a;
+		*a = *b;
+		*b = safu;
+}
