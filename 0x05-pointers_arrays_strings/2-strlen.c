09d1525116b6001da4dffe3ae94a2ed286f6ecf2
safumulatu <safumulatu9@gmail.com>
2023-09-18 03:55:46 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - function that returns length of string
+ *
+ * @s: character of strings
+ *
+ * Return: (0)
+ * owned by Safu 
+ */
+int _strlen(char *s)
+{
+	int total_len;
+
+	total_len = 0;
+	while (s[total_len] != '\0')
+	{
+	total_len++;
+	}
+
+	return (total_len);
+}
