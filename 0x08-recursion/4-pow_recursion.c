a0acb9c50824893f41f5177a00cdad292b15eb3a
safumulatu <safumulatu9@gmail.com>
2023-09-29 07:05:08 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,28 @@
-#include "main.h"
+/*
+ * File: 4-pow_recursion.c
+ * Auth: s m
+ */
+
+#include "holberton.h"
 
 /**
  * _pow_recursion - Returns the value of x raised to the power of y.
- * @x: The base.
- * @y: The exponent.
+ * @x: The number to be raised.
+ * @y: The power.
  *
- * Return: The result of x^y, or -1 if y is negative.
+ * Return: The value of x raised to the power of y.
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0) // Check for negative exponent
-    {
-        return -1; // Return -1 to indicate an error
-    }
-    else if (y == 0) // Base case: Any number raised to the power of 0 is 1
-    {
-        return 1;
-    }
-    else
-    {
-        return x * _pow_recursion(x, y - 1); // Recursive call to compute power
-    }
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
