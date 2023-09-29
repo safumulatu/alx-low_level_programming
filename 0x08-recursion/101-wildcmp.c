285f7c0f4d3c76890677517f2981cbe865166641
safumulatu <safumulatu9@gmail.com>
2023-09-29 07:18:32 -0700

    Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,38 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * wildcmp - Checks if two strings are identical considering '*' as wildcard.
+ * @s1: The first string.
+ * @s2: The second string with possible wildcard '*'.
  *
- * Return: Always 0.
+ * Return: 1 if the strings are identical, 0 otherwise.
  */
-int main(void)
+int wildcmp(char *s1, char *s2)
 {
-	int r;
+    if (*s1 == '\0' && *s2 == '\0') // Base case: both strings are empty, consider them identical
+        return (1);
 
-	r = wildcmp("main.c", "*.c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m*a*i*n*.*c*");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "main.c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m*c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "ma********************************c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "*");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "***");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m.*c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "**.*c");
-	printf("%d\n", r);
-	r = wildcmp("main-main.c", "ma*in.c");
-	printf("%d\n", r);
-	r = wildcmp("main", "main*d");
-	printf("%d\n", r);
-	r = wildcmp("abc", "*b");
-	printf("%d\n", r);
-	return (0);
+    if (*s2 == '*')
+    {
+        if (*s1 == '\0') // If s1 is empty, skip '*' in s2
+            return (wildcmp(s1, s2 + 1));
+        // Try matching s1 with '*' in s2 or skip '*' in s2
+        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+    }
+
+    if (*s1 == *s2)
+        return (wildcmp(s1 + 1, s2 + 1)); // Characters match, continue comparison
+
+    return (0); // Characters do not match
 }
+
