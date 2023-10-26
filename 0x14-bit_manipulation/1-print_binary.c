10b2382f7ce5d7cd6b5d6052f4b2ba52111d1040
safumulatu <safumulatu9@gmail.com>
2023-10-26 09:01:29 -0700

    binary tree

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,113 @@
+#include "main.h"
 #include <stdio.h>
 
-void print_binary_representation(unsigned int num) {
-    if (num == 0) {
-        printf("0"); // If the number is zero, its binary representation is just "0".
-        return;
-    }
+/*
+ * Description - Debug option: To enable debugging, define DEBUG macro.
+ *                For checker validation, use the checker version in the readme.
+ */
 
-    int binary[32]; // Assuming 32-bit integers
-    int i = 0;
-
-    while (num > 0) {
-        binary[i] = num % 2;
-        num /= 2;
-        i++;
-    }
-
-    for (i--; i >= 0; i--) {
-        printf("%d", binary[i]);
-    }
+/**
+ * debug_1 - Print debug statements
+ *
+ * @n: Value of n
+ * @length: Length of n
+ * @mask: Mask of n
+ */
+void debug_1(unsigned long int n, int length, unsigned long int mask)
+{
+	#ifdef DEBUG
+	printf("Length of %lu (n) is %i and mask (original value 1) is %lu based on [length of n - 1] == %i.\n\n", n, (length + 1), mask, length);
+	#endif
 }
 
-int main() {
-    unsigned int num = 42; // Replace with your desired number
-    printf("Binary representation of %u: ", num);
-    print_binary_representation(num);
-    printf("\n");
+/**
+ * debug_2 - Print debug statements
+ *
+ * @n: Value of n
+ * @mask: Mask of n
+ */
+void debug_2(unsigned long int n, unsigned long int mask)
+{
+	#ifdef DEBUG
+	printf("Value of n is %lu, value of mask is %lu, and value of [n & mask] is %lu.\n\n", n, mask, (n & mask));
+	#endif
+}
 
-    return 0;
+/**
+ * debug_3 - Print debug statements
+ *
+ * @mask: Mask of value n
+ */
+void debug_3(unsigned long int mask)
+{
+	#ifdef DEBUG
+	printf("Value of mask is %lu after right shifting by one.\n\n", mask);
+	#endif
+}
+
+/**
+ * _length - Find the length of @n
+ *
+ * @n: Value to find its length
+ * Return: Length
+ */
+int _length(unsigned long int n)
+{
+	int length = 0;
+
+	while (n > 0)
+	{
+		#ifdef DEBUG
+		printf("Value of n is %lu before right shifting by one.\n\n", n);
+		#endif
+
+		length++;
+		n >>= 1; /* Shift n to the right by 1 */
+
+		#ifdef DEBUG
+		printf("Value of n is %lu after right shifting by one.\n\n", n);
+		#endif
+	}
+
+	length--;
+
+	return (length);
+}
+
+/**
+ * print_binary - A function that converts a decimal to binary
+ *
+ * @n: Decimal number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	int length;
+	unsigned long int mask = 1;
+
+	length = _length(n);
+
+	if (length > 0) /* Create mask based on the length of the number */
+		mask <<= length; /* Shift mask to the left by length */
+
+	#ifdef DEBUG
+	debug_1(n, length, mask);
+	#endif
+
+	while (mask > 0)
+	{
+		#ifdef DEBUG
+		debug_2(n, mask);
+		#endif
+
+		if (n & mask) /* If n & mask == 1, print 1 */
+			_putchar('1');
+		else /* Else if n & mask == 0, print 0 */
+			_putchar('0');
+
+		mask >>= 1; /* Shift mask to the right by 1 */
+
+		#ifdef DEBUG
+		debug_3(mask);
+		#endif
+	}
 }
